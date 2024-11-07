dc5dc28c4af3e8e6204fd42e2cfbf62813944084
noorfatima333 <noor.fatima@codedistrict.com>
2024-11-07 17:54:02 +0500

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 
 function App() {
+  const showGenreList = useBreakpointValue({ base: false, lg: true });
+
   return (
     <>
       <Grid
@@ -11,16 +13,22 @@ function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`,
         }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
       >
-        <GridItem area="main">
-          <GameGrid></GameGrid>
-        </GridItem>
-        <GridItem area="aside">
-          <GenreList />
-        </GridItem>
         <GridItem area="nav">
           <NavBar />
         </GridItem>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
+        {showGenreList && (
+          <GridItem area="aside" paddingX={"5px"}>
+            <GenreList />
+          </GridItem>
+        )}
       </Grid>
     </>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        gap={10}
+        gap={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
