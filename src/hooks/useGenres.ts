55736da96c9a3cb854bc989c7474bdb832cada2c
noorfatima333 <noor.fatima@codedistrict.com>
2024-11-19 13:33:35 +0500

    Shipping static genre list within the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,9 @@
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, error: null, isLoading: false });
 export default useGenres;
